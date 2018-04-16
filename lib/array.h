19bd42b54683ecda3b9ffacc0c25390a81af31ac
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2018-04-16 23:19:43 +0300

    Implement templates
    Implemented #14

--- lib/array.h
+++ lib/array.h
@@ -6,38 +6,195 @@
 #ifndef array_h
 #define array_h
 
+
+template<typename T>
+class Array;
+
+template<typename T>
+std::ostream &operator<<(std::ostream &, const Array<T> &);
+
+
+template <typename T>
 class Array {
 private:
-  list Map;
+  list<T> Map;
   int range;
-  void set(int, int);
-  int get(int) const;
+
+
+  // Set value for element with known index
+  // (This will be replaced with operator[])
+  //   index - an abstract index of element
+  //           in array
+  //   value - value to be set for this element
+  //
+  void set(int index, T value) {
+    if (index + 1 > length) length = index + 1;
+
+    int slice_number = floor(index / range);
+    int index_in_slice = index - slice_number * range;
+
+    if (index + 1 > Map.length * range) {
+      T *new_slice = new T[range];
+      new_slice[index_in_slice] = value;
+      Map.push(new_slice);
+    } else {
+      T *slice = Map.get(slice_number);
+      slice[index_in_slice] = value;
+    }
+  };
+
+
+  // Get an element value by its index
+  //   index - an abstract index of element
+  //           in array
+  //
+  T get(int index) const {
+    int slice_number = floor(index / range);
+    int index_in_slice = index - slice_number * range;
+
+    if (index + 1 > length) {
+      std::cout << "Index is out of bounds" << std::endl;
+      exit(1);
+    }
+
+    T *slice = Map.get(slice_number);
+    return slice[index_in_slice];
+  };
+
+  
 public:
-  Array(int);
   int length;
 
-  void push(int);
-  int pop();
-  Array map(int (*)(int));
-  Array filter(bool (*)(int));
-  std::string join(const char*) const;
-  std::string to_string() const;
+  // Array constructor
+  //   size - size of one slice
+  //          of array
+  //
+  Array(int size) {
+    T *init_slice = new T[size];
+    range = size;
+    Map.push(init_slice);
+    length = 0;
+  };
 
-  friend std::ostream &operator<<(std::ostream &, const Array&);
+
+  // Push element to array
+  //   value - value to be placed
+  //           in the end of array
+  //
+  void push(T value) {
+    int index = length;
+    set(index, value);
+  };
+
+
+  // Get last element and delete
+  // it from array
+  //
+  T pop() {
+    T popped = get(length - 1);
+    length--;
+
+    if (Map.length * range - 1 == length) Map.pop();
+
+    return popped;
+  };
+
+
+  // Project array into new one
+  // through the function
+  //   fn - function to be applied
+  //        to each element
+  //
+  Array<T> map(T (*fn)(T)) {
+    Array<T> mapped (length);
+
+    for (int i = 0; i < length; ++i) {
+      T item = get(i);
+      T value = fn(item);
+      mapped.push(value);
+    }
+
+    return mapped;
+  };
+
+
+  // Project array into new one
+  // filtering by the function
+  //   fn - function which returns
+  //        true or false
+  //
+  Array<T> filter(bool (*fn)(T)) {
+    Array<T> filtered (length);
+
+    for (int i = 0; i < length; ++i) {
+      T item = get(i);
+      T value = fn(item);
+      if (value) filtered.push(item);
+    }
+
+    return filtered;
+  };
+
+
+  // Create string from elements of array
+  // separated by specified separator
+  //
+  std::string join(const char *separator) const {
+    std::string str;
+
+    if (!length) return str;
+
+    int i;
+    for (i = 0; i < length - 1; ++i) {
+      T item = get(i);
+      std::string sep (separator);
+      str += std::to_string(item) + sep;
+    }
+
+    str += std::to_string(get(i));
+    return str;
+  };
+
+
+  // Represent array as a string
+  //
+  std::string to_string() const {
+    std::string str ("[");
+
+    if (!length) {
+      str += "]";
+      return str;
+    }
+
+    str += join(", ") + "]";
+    return str;
+  };
 
 
   struct Proxy {
-    Array& a;
+    Array<T> &arr;
     int index;
-    Proxy(Array &a, int index) : a(a), index(index) {}
+    Proxy(Array &arr, int index) : arr(arr), index(index) {}
 
-    operator int() { return a.get(index); }
-    void operator=(int other) { a.set(index, other); }
+    operator T() { return arr.get(index); }
+    void operator=(T value) { arr.set(index, value); }
   };
 
   Proxy operator[](int index) {
     return Proxy(*this, index);
   }
+
+  friend std::ostream &operator<< <T> (std::ostream &, const Array<T> &);
 };
+
+
+// Operator for cout
+//
+template<typename T>
+std::ostream &operator<<(std::ostream &os, Array<T> const &arr) {
+  os << arr.to_string();
+  return os;
+}
+
 
 #endif
--- lib/list.h
+++ lib/list.h
@@ -1,18 +1,102 @@
+#include <iostream>
 #ifndef list_h
 #define list_h
 
-struct list_node;
 
+template<typename T>
+struct node {
+  T *slice;
+  node *next;
+};
+
+template<typename T>
 class list {
 private:
-  list_node *head, *tail;
+  node<T> *head, *tail;
 public:
-  list();
+
+
   int length;
-  void push(int *);
-  void pop();
-  void clear();
-  int *get(int) const;
+
+  // List constructor
+  //
+  list() {
+    head = NULL;
+    tail = NULL;
+    length = 0;
+  };
+
+
+  // Push element to the list
+  //   slice - an array allocated
+  //           with new T[]
+  //
+  void push(T *slice) {
+    node<T> *temp;
+    temp->slice = slice;
+    temp->next = NULL;
+
+    if (!head) {
+      head = temp;
+      tail = temp;
+    } else {
+      tail->next = temp;
+      tail = tail->next;
+    }
+
+    length++;
+  };
+
+
+  // Delete last element from the list
+  //
+  void pop() {
+    if (!head) return;
+
+    node<T> temp = head;
+
+    while (temp->next->next != tail) temp = temp->next;
+
+    delete temp->next->slice;
+    delete temp->next;
+
+    tail = temp;
+    tail->next = NULL;
+
+    length--;
+  };
+
+
+  // Clear list
+  //
+  void clear() {
+    node<T> temp;
+    temp = head;
+
+    while (head) {
+      temp = head;
+      head = head->next;
+      delete temp->slice;
+      delete temp;
+    }
+
+    length = 0;
+  };
+
+
+  // Get slice by its number in list
+  //   slice_number - number of slice
+  //                  in the list
+  //
+  T *get(int slice_number) const {
+    node<T> temp = head;
+
+    for (int i = 0; i < slice_number; ++i) {
+      temp = temp->next;
+    }
+
+    return temp->slice;
+  };
 };
 
 #endif
